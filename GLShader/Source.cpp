c3803ee8ab2f7d0af043d16e1cb100530ee34594
JoeFrumenti <joefrumenti@gmail.com>
2022-12-20 09:51:17 -0800

    backing up before i break everything

--- GLShader/Source.cpp
+++ GLShader/Source.cpp
@@ -1,5 +1,6 @@
 #include <Glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <OpenGL/Shader.h>
 #include <iostream>
 
 
@@ -178,6 +179,9 @@ int main()
 
 	glUseProgram(shaderProgram);
 
+	Shader ourShader("vertexshader.vert", "fragmentshader.frag");
+	//ourShader.use();
+
 	//RENDER LOOP
 	while (!glfwWindowShouldClose(window))
 	{
@@ -190,11 +194,11 @@ int main()
 
 
 		//draw triangle
-		float timeValue = glfwGetTime();
-		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		//float timeValue = glfwGetTime();
+		//float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		//int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
 		
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
